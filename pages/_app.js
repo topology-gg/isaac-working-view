d1fb63f5a3e461831d3c0f2b02970bbc79eaae79
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-08-02 22:30:14 +0800

    set title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import NextHead from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+        <NextHead>
+            <title>Isaac: Working View</title>
+        </NextHead>
+        <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
