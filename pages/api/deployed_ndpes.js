78a3676848c4d86d250f5a5c87be99c7d1f946ef
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-08-16 10:51:03 +0100

    update db name; stop loading jsons for perlin

--- pages/api/deployed_ndpes.js
+++ pages/api/deployed_ndpes.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
     const client = await clientPromise
 
-    const db = client.db('isaac')
+    const db = client.db('isaac_10ce37b')
     const deployed_ndpes = await db
         .collection('u0' + '_deployed_ndpes')
         .find({'_chain.valid_to' : null})
