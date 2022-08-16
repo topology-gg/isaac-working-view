5b7e576acfff4a9701ad124ecda417a07c249014
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-08-16 15:21:15 +0100

    third pass not working

--- components/GameStatsPlayers.js
+++ components/GameStatsPlayers.js
@@ -30,32 +30,18 @@ export default function GameStatsPlayers(props) {
         empty_result_array.push (null)
     }
     const [accountStringsState, setAccountStringsState] = useState(empty_addr_array)
-    const [snsResults, setSnsResults] = useState()
+    // const [snsResults, setSnsResults] = useState(empty_result_array)
+    var snsResults = empty_result_array
     const [rowsState, setRowsState] = useState([])
 
-    // for (var i=0; i<CIV_SIZE; i++) {
-    //     const { data, loading, error, refresh} = useStarknetCall ({
-    //         contract: snsContract,
-    //         method: 'sns_lookup_adr_to_name',
-    //         args: [accountStringsState[i]]
-    //     })
-    // }
-
     useEffect (() => {
-        if (!db_player_balances) return;
-        if (!db_player_balances.player_balances) return;
-
-        console.log('GameStatsPlayers:', db_player_balances)
-        // const population = db_player_balances.player_balances.length
+        if (snsResults.includes(null)) return;
 
         var rows = []
-        for (var row_idx = 0; row_idx < CIV_SIZE; row_idx ++){
-            const account_str = toBN(db_player_balances.player_balances[row_idx]['account']).toString(16)
-            // setAccountStringsState( accountStringsState.concat([account_str]) )
+        for (var i = 0; i < CIV_SIZE; i++){
+            const account_str = accountStringsState[i]
             const account_str_abbrev = "0x" + account_str.slice(0,3) + "..." + account_str.slice(-4)
-
-            //const [exist, name_string] = parse_call_result (...)
-            const exist = 0
+            const [exist, name_string] = parse_call_result (snsResults[i])
             const display_account_str = exist == 0 ? account_str_abbrev : name_string
 
             var cell = []
@@ -80,7 +66,61 @@ export default function GameStatsPlayers(props) {
         }
         setRowsState (rows)
 
-    }, [db_player_balances, account])
+    }, [snsResults, account])
+
+    for (var i=0; i<CIV_SIZE; i++) {
+        const { data, loading, error, refresh} = useStarknetCall ({
+            contract: snsContract,
+            method: 'sns_lookup_adr_to_name',
+            args: [accountStringsState[i]]
+        })
+        snsResults[i] = data
+        // var newSnsResults = snsResults
+        // newSnsResults[i] = data
+        // setSnsResults (newSnsResults)
+    }
+
+    useEffect (() => {
+        if (!db_player_balances) return;
+        if (!db_player_balances.player_balances) return;
+
+        console.log('GameStatsPlayers:', db_player_balances)
+        // const population = db_player_balances.player_balances.length
+
+        for (var row_idx = 0; row_idx < CIV_SIZE; row_idx ++){
+            const account_str = toBN(db_player_balances.player_balances[row_idx]['account']).toString(16)
+            setAccountStringsState( accountStringsState.concat([account_str]) )
+        }
+
+        //     const account_str_abbrev = "0x" + account_str.slice(0,3) + "..." + account_str.slice(-4)
+
+        //     //const [exist, name_string] = parse_call_result (...)
+        //     const exist = 0
+        //     const display_account_str = exist == 0 ? account_str_abbrev : name_string
+
+        //     var cell = []
+        //     cell.push (<td key={`players-rowidx-${row_idx}`}>{row_idx}</td>)
+
+        //     if (!account) {
+        //         cell.push (<td key={'players-not-account'}>{display_account_str}</td>)
+        //     }
+        //     else {
+        //         // check if signed-in account matches current row
+        //         const signed_in_account_str = toBN(account).toString(16)
+        //         // console.log (`account_str: ${account_str}; signed_in_account_str: ${signed_in_account_str}`)
+        //         if (account_str === signed_in_account_str) {
+        //             cell.push (<td key={'players-account-signedin'} style={{color:'#00CCFF'}}>{display_account_str}</td>)
+        //         }
+        //         else {
+        //             cell.push (<td key={'players-account-not-signedin'}>{display_account_str}</td>)
+        //         }
+        //     }
+
+        //     rows.push(<tr key={`players-row-${row_idx}`} className="player_account">{cell}</tr>)
+        // }
+        // setRowsState (rows)
+
+    }, [db_player_balances])
 
 
     //
