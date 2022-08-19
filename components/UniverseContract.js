2c15b5d6e1f0fd4c61714834dfd65dcd7d7264ae
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-08-19 18:49:30 +0100

    update univ address to open alpha

--- components/UniverseContract.js
+++ components/UniverseContract.js
@@ -3,7 +3,7 @@ import {
 } from '@starknet-react/core'
 
 import UniverseAbi from '../abi/universe_abi.json'
-const UNIVERSE_ADDR = '0x02b1a7651d9d0d3a434330a0dc14be766b190a588be29f3864f32e2fb3c3e21a' // universe #0
+const UNIVERSE_ADDR = '0x03df9fa61c7f69d0b9e5da0ed94ceafed7c6f9ffa56b3828d515768ef861bb56' // universe #0
 
 export function useUniverseContract () {
     return useContract ({ abi: UniverseAbi, address: UNIVERSE_ADDR })
